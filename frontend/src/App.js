232ac00085ed4ee1d9e8030056dfc6164b72fe18
Jen-kins <60917179+Jen-kins@users.noreply.github.com>
2020-09-30 20:39:55 +0200

    React-initial-setup-front-end-start

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>Welcome to the e-commerce party</h1>
+    </Fragment>
+  );
+};
+
+export default App;
