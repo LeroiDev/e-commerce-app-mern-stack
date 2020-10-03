42ade523a5dc3e7e11db76a096e0e72ad2356816
Jen-kins <60917179+Jen-kins@users.noreply.github.com>
2020-10-04 00:08:22 +0200

    productscreen-component-next-backend-setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,24 @@ import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreem from "./screens/ProductScreen";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 const App = () => {
   return (
-    <Fragment>
-      <Header />
+    <Router>
+      <Fragment>
+        <Header />
 
-      <main className="py-3">
-        <Container>
-          <h1>Welcome to the e-commerce party</h1>
-          <HomeScreen />
-        </Container>
-      </main>
-      <Footer />
-    </Fragment>
+        <main className="py-3">
+          <Container>
+            <h1>Welcome to the e-commerce party</h1>
+            <Route exact path="/" component={HomeScreen} />
+            <Route path="/product/:id" component={ProductScreem} />
+          </Container>
+        </main>
+        <Footer />
+      </Fragment>
+    </Router>
   );
 };
 
